33cdf93868b75ef683aab133fc938333987d1105
Gabriel Gomes <gabrielgomes639@gmail.com>
2017-09-27 12:34:44 -0300

    rule

--- build/pages/register/src/RegisterController.php
+++ build/pages/register/src/RegisterController.php
@@ -7,6 +7,16 @@
 	$email = $_REQUEST['email'];
 	$rule = 0;
 
+	$sql ="SELECT * FROM user";
+	$result = mysql_query($sql);
+
+
+	if(mysql_num_rows($result) > 0) {
+		$rule = 0;
+	} else {
+		$rule = 2;
+	}
+
 
 	$sql ="INSERT INTO user (`token`, `firstname`, `lastname`, `email`, `rule`)
 	VALUES ('$token', '$firstName', '$lastName', '$email', '$rule')";
